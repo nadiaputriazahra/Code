206c52a67c86b412efc7aad6f45137922415577a
Nadia Putri A. <nadiaajja673@gmail.com>
2025-12-19 17:43:36 +0700

    prak2
    Added a welcome message and personal details.

--- PengenalanPemrograman/Praktikkum2/cocalc.cpp
+++ PengenalanPemrograman/Praktikkum2/cocalc.cpp
@@ -1,1 +1,20 @@
+/*Program ini dibuat saat pertama kali Praktikum
+Mata kuliah Pengenalan Pemrograman. Program ini
+dibuat menggunakan platfrom online yaitu
+cocalc.com*/
 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Nadia Putri Azahra";
+    string NPM = "2517051034";
+
+    cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+    cout << "Nama saya Nadia Putri Azahra" << nama << endl;
+    cout << "NPM saya 2517051034" << NPM << endl;
+
+    cout << "Unila Indah" << endl;
+    cout << "Be Strong" << endl;
+}
