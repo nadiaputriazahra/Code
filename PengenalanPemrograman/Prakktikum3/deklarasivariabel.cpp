244c383ed86267328c18e079bd7eb64417287b4a
Nadia Putri A. <nadiaajja673@gmail.com>
2025-12-19 17:46:12 +0700

    Add variable declarations in deklarasivariabel.cpp

---
+++ PengenalanPemrograman/Prakktikum3/deklarasivariabel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool b; //deklarasi variabel dengan nama "b" bertipe data boolean
+    int i; //deklarasi variabel dengan nama "i" bertipe data int
+    float f; //deklarasi variabel dengan nama "f" bertipe data float
+    double d; // deklarasi variabel dengan nama "d" bertipe data double
+    char c; //deklarasi variabel dengan nama "c" bertipe data char
+    string s; // deklarasi variabel dengan nama "s" bertipe data string
+
+    return 0;
+}
