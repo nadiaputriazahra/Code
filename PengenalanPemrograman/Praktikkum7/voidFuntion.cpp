83671d063cf4aefb22c3e56b9ad984e442798ea1
Nadia Putri A. <nadiaajja673@gmail.com>
2025-12-19 17:52:15 +0700

    Add voidFuntion.cpp to introduce a person

---
+++ PengenalanPemrograman/Praktikkum7/voidFuntion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void perkenalkan(){
+    cout << "Hi, nama saya adalah Nadia Putri Azahra" << endl;
+    cout << "Saya berasal dari Program Studi Ilmu Komputer" << endl;
+    cout << "Saya dari kelas A" << endl;
+}
+
+int main(){
+    perkenalkan();
+
+    return 0;
+}
