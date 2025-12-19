cb7eb7ed5d672165a0382a9e163411484b89c0b1
Nadia Putri A. <nadiaajja673@gmail.com>
2025-12-19 17:46:52 +0700

    Add inisialisasi.cpp with variable declarations

---
+++ PengenalanPemrograman/Prakktikum3/inisialisasi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool b = true;
+    int i = 5;
+    float f = 3.14;
+    double d = 3.141592;
+    char c = 'A';
+    string s; "Selamat Pagi";
+
+    return 0;
+}
