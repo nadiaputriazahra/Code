a7c9f0b8699e2674059ed81aaa5a67fab190686f
Nadia Putri A. <nadiaajja673@gmail.com>
2025-12-19 18:10:38 +0700

    Add file to append text to a file

---
+++ PengenalanPemrograman/Praktikkum11/menambahisiFileText.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ofstream myFile;
+    string teks;
+
+    cout << "Masukkan teks yang ingin ditambahkan: ";
+    getline(cin, teks);
+
+    myFile.open("data.txt", ios::app);
+    myFile << teks;
+
+    cout << "Teks berhasil ditambahkan!" << endl;
+
+    myFile.close();
+}
